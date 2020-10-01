ada5c995b808379cc4e06d396ebefad3c84a8569
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-10-01 17:33:23 +0300

    formData example

---
+++ formData/formData.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleSubmitFormData);
+
+
+function handleSubmitFormData(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.currentTarget);
+  const data = {};
+
+  formData.forEach((name, value) => {
+    data[name] = value;
+  });
+    
+    return data;
+}
