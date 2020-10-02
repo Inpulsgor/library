be1b8e49c0ef213c1f0e8e544193b05b0a04b5c7
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-10-02 11:02:04 +0300

    Create handleChange.js

---
+++ handleChange/handleChange.js
@@ -0,0 +1,7 @@
+  handleChange = ({ target }) => {
+    const { name, value, type, checked } = target;
+
+    // Если тип элемента checkbox, берем значение checked,
+    // в противном случае value
+    this.setState({ [name]: type === 'checkbox' ? checked : value });
+  };
