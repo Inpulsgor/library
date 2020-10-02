c6e78ffc08672f06ad6f7bf98a8348fa01fa338d
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-10-02 09:24:07 +0300

    Create activeLink.js

---
+++ activeLink/activeLink.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector('.js-nav');
+nav.addEventListener('click', handleClick);
+
+function handleClick(e) {
+  e.preventDefault(); // клік по посиланню перезавантажує сторінку
+
+  if (e.target === e.currentTarget) {
+    return;
+  }
+
+  const currentLink = e.target;
+  const activeLink = e.currentTarget.querySelector(".nav__link--active");
+
+  if (activeLink) {
+    activeLink.classList.remove("nav__link--active"); // знімає активний клас - якщо такий вже є
+  }
+
+  currentLink.classList.add("nav__link--active"); // додає активний клас
+}
