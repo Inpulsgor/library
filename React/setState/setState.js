ac7a8f4c50009c31d47a9e0a3413c8026d473f50
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-12-10 12:37:58 +0200

    Create setState.js

---
+++ React/setState/setState.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Test extends Component {
+  state = {
+    tasks: [],
+  };
+
+  addTask = (task) => {
+    this.setState((prevState) => {
+      return {
+        tasks: [...prevState.tasks, task],
+      };
+    });
+  };
+
+  render() {
+    const { tasks } = this.state;
+
+    return (
+      <>
+        <button onClick={this.addTask}>Add</button>
+        {tasks.length > 0 && <TaskList tasks={tasks} />}
+      </>
+    );
+  }
+}
+
+export default Test;
