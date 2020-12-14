0eab5049cceb21149cd5f4ec6aedd0f28f1385cb
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-12-14 11:31:52 +0200

    Create helpers.js

---
+++ React/helpers/helpers.js
@@ -0,0 +1,9 @@
+// функция-помощник, которая возвращает массив обьектов такого формата, который ожидает компонент
+
+const mapper = (articles) => {
+  return articles.map(({ objectID: id, url: link, ...props }) => ({
+    id,
+    link,
+    ...props,
+  }));
+};
