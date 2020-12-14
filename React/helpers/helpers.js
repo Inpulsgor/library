93f4d760a675c3090d1e6b0ab77e611a919850e9
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-12-14 11:34:48 +0200

    Update helpers.js

--- React/helpers/helpers.js
+++ React/helpers/helpers.js
@@ -1,5 +1,4 @@
 // функция-помощник, которая возвращает массив обьектов такого формата, который ожидает компонент
-
 const mapper = (articles) => {
   return articles.map(({ objectID: id, url: link, ...props }) => ({
     id,
@@ -7,3 +6,10 @@ const mapper = (articles) => {
     ...props,
   }));
 };
+
+// Проверка массива на длину 
+export const isEmpty = (value) =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
