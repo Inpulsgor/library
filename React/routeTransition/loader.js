f65b88137b2002e457b60efe09a5afd0d92c58c9
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-01-29 13:28:57 +0200

    Create loader.js

---
+++ React/routeTransition/loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { CommonLoading } from "react-loadingg";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <CommonLoading color="orange" size="large" />
+    </div>
+  );
+};
+
+export default Loader;
