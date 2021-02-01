4c2954403c9ba7aec5e50eca17c442012573d3bb
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-02-01 11:23:06 +0200

    Create index.js

---
+++ React/Redux/redux-toolkit/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "redux/store";
+import App from "components/App/App";
+import "scss/main.scss";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
+);
