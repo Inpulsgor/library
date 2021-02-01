6702ebd91b0da64e755d554a2c3c38368ce1b1de
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-02-01 11:23:19 +0200

    Create store.js

---
+++ React/Redux/redux-toolkit/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore } from "redux-persist";
+import { logger } from "redux-logger";
+import thunk from "redux-thunk";
+
+import rootReducer from "./rootReducer";
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: [thunk, logger],
+});
+
+export const persistor = persistStore(store);
